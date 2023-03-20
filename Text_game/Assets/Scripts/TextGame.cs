c4e4bf8761471b34c3c55a9a9914522552099506
Julia Klim <93789683+Jklim447@users.noreply.github.com>
2023-03-20 20:53:54 +0100

    added new project with text game

---
+++ Text_game/Assets/Scripts/TextGame.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int min = 1;
+    int max = 1000;
+    int shoot = 500;
+    void Start()
+    {
+        
+        Debug.Log("Witam w grze- pomyœl w g³owie liczbê od 1 do 1000");
+        Debug.Log("Czy twoja liczba jest równa/wiêksza/mniejsza od 500?");
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = shoot;
+            shoot = (min + max) / 2;
+            Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od "+shoot + "?");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+           max = shoot;
+            shoot = (min + max) / 2;
+            Debug.Log("Czy twoja liczba jest rowna/wieksza/mniejsza od " + shoot + "?");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("KOMPUTER ZGADL LICZBE");
+        }
+    }
+}
